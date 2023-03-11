d6048b326c2ffb8f12c7d40ca27a7e0264631a7d
Axel Jäger <axeljaeger@googlemail.com>
2023-03-11 11:21:23 +0100

    Convert to standalone.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
