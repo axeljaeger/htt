758aa2fdc8e36700428ce219a76291a1db623e21
Axel Jäger <axeljaeger@gmail.com>
2024-03-27 08:44:07 +0100

    Implement render on demand.

--- src/app/add-transformations/add-transformations.component.ts
+++ src/app/add-transformations/add-transformations.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -9,15 +9,15 @@ export enum TransformationType {
   Translation = 'Translation'
 }
 
-
 @Component({
   selector: 'app-add-transformations',
   standalone: true,
   imports: [CommonModule, MatButtonModule],
   templateUrl: './add-transformations.component.html',
-  styleUrls: ['./add-transformations.component.css']
+  styleUrls: ['./add-transformations.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddTransformationsComponent {
   public TransformationType = TransformationType;
-  @Output() public addTransformation = new EventEmitter<TransformationType>();
+  public addTransformation = output<TransformationType>();
 }
