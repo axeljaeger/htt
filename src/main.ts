cb9fafe9ec1acd6aa9fd347a5c697eae2fdf3606
Axel Jäger <axeljaeger@gmail.com>
2025-04-18 16:52:24 +0200

    Remove zone.js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 
 bootstrapApplication(AppComponent, {
     providers: [
-        provideAnimations()
+        provideAnimations(),
+        provideExperimentalZonelessChangeDetection(),
     ]
 })
   .catch(err => console.error(err));
