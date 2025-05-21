e0d91de371134fdd6dcfab204d49e20ee2670bc7
Axel Jäger <axeljaeger@gmail.com>
2025-05-21 07:51:46 +0200

    Cleanup migration to SVG

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
-
 bootstrapApplication(AppComponent, {
     providers: [
-        provideAnimations(),
         provideExperimentalZonelessChangeDetection(),
     ]
 })
