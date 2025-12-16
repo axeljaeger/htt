c53743c8c922d006c12abb5b6e107eb11ac1c284
Axel Jäger <axeljaeger@gmail.com>
2025-12-16 20:19:24 +0100

    Use new host binding syntax

--- src/app/add-transformations/add-transformations.component.ts
+++ src/app/add-transformations/add-transformations.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, effect, HostBinding, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 
 export type TransformationType = 'Rotation' | 'Scaling' | 'Shearing' | 'Translation';
 
@@ -6,13 +6,12 @@ export type TransformationType = 'Rotation' | 'Scaling' | 'Shearing' | 'Translat
     selector: 'app-add-transformations',
     templateUrl: './add-transformations.component.html',
     styleUrls: ['./add-transformations.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+      '[style.--accent]': 'color()'
+    }
 })
 export class AddTransformationsComponent {
-  @HostBinding('style.--accent') backgroundColor = 'lightblue';
   color = input<string>();
-  updateCss = effect(() => this.backgroundColor = this.color());
-
   public addTransformation = output<TransformationType>();
-
 }
